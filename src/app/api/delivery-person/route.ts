f2c82114ee1cf3113dd670d04c0011c61d47960a
shaktiDD <shaktidhardubey7@gmail.com>
2024-08-28 11:34:39 +0530

    get all delivery

--- src/app/api/delivery-person/route.ts
+++ src/app/api/delivery-person/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db/db";
-import { deliveryPersons } from "@/lib/db/schema";
+import { deliveryPersons, warehouses } from "@/lib/db/schema";
 import { deliveryPersonSchema } from "@/lib/validators/deliveryPerson";
+import { desc, eq } from "drizzle-orm";
 
 export async function POST(req: Request) {
     const requestData = await req.json();
@@ -15,5 +16,21 @@ export async function POST(req: Request) {
         return Response.json({message: "Delivery person stored successfully"}, {status: 201});
     }catch(err){
         return Response.json({message: "Failed to store delivery person"}, {status: 500});
+    }
+}
+
+export async function GET(req: Request) {
+    try{
+        const allDeliveryPersons = await db.select({
+            id: deliveryPersons.id,
+            name: deliveryPersons.name,
+            phone: deliveryPersons.phone,
+            warehouse: {
+                name: warehouses.name
+            }
+        }).from(deliveryPersons).leftJoin(warehouses,eq(deliveryPersons.warehouseId,warehouses.id)).orderBy(desc(deliveryPersons.id));
+        return Response.json(allDeliveryPersons, {status: 200});
+    }catch(err){
+        return Response.json({message: "Failed to fetch delivery persons"}, {status: 500});
     }
 }
